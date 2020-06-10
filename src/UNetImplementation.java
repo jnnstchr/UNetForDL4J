fc63577e2406a711e030547925ca14a9824d0d06
Joanna Stachera <j.k.stachera@student.tudelft.nl>
2020-06-10 14:26:47 +0200

    Successfully adds pretrainedNet

--- src/UNetImplementation.java
+++ src/UNetImplementation.java
@@ -6,12 +6,21 @@ import org.datavec.api.split.FileSplit;
 import org.datavec.image.loader.NativeImageLoader;
 import org.datavec.image.recordreader.ImageRecordReader;
 import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.ComputationGraphConfiguration;
+import org.deeplearning4j.nn.graph.ComputationGraph;
+import org.deeplearning4j.nn.transferlearning.FineTuneConfiguration;
 import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.zoo.PretrainedType;
+import org.deeplearning4j.zoo.ZooModel;
+import org.deeplearning4j.zoo.model.UNet;
+import org.deeplearning4j.common.resources.DL4JResources;
 import org.nd4j.linalg.dataset.api.DataSet;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
 import org.nd4j.linalg.dataset.api.preprocessor.ImagePreProcessingScaler;
 import org.nd4j.linalg.dataset.api.preprocessor.NormalizerMinMaxScaler;
+import org.nd4j.linalg.learning.config.Nesterovs;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
@@ -56,11 +65,11 @@ public class UNetImplementation {
         scaler.fitLabel(true);
         scaler.fit(dataTrainIter);
         dataTrainIter.setPreProcessor(scaler);
+        DL4JResources.setBaseDownloadURL("https://dl4jdata.blob.core.windows.net/");
+        ZooModel zooModel = UNet.builder().build();
+        ComputationGraph pretrainedNet = (ComputationGraph) zooModel.initPretrained(PretrainedType.SEGMENT);
+        System.out.println(zooModel);
 
-        for (int i = 1; i < 3; i++) {
-            DataSet ds = dataTrainIter.next();
-            System.out.println(ds);
-            System.out.println(dataTrainIter.getLabels());
-        }
+
     }
     }
